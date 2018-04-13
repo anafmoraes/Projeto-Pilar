3b00f77ea0ced407fc2924e9b9dae77ac613f7d2
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2018-04-13 10:05:36 -0300

    Correção de chamadas de arquivos.js

--- application/views/frontend/template/header.php
+++ application/views/frontend/template/header.php
@@ -1,7 +1,7 @@
 	<!-- Script para atualizar a página atual 1 vez (evita erro de reenvio de formulário) -->
 	<!-- You can call this via the body tag if desired -->
 	<script language="JavaScript" type="text/javascript">
-
+		/*
 		var reloaded = false;
 		var loc=""+document.location;
 		loc = loc.indexOf("?reloaded=")!=-1?loc.substring(loc.indexOf("?reloaded=")+10,loc.length):"";
@@ -13,6 +13,7 @@
 			window.location.replace(window.location+"?reloaded=true");
 		}
 		reloadOnceOnly();
+		*/
 	</script>
 	
 	<!-- Início do Header (Barra de Menus) -->
