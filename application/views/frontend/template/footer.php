6f1dba73755d05523fef37a750f7819ca84170c4
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2018-05-11 15:01:09 -0300

    Modal OK, Página Inicial Antes Login e após login em andamento

--- application/views/frontend/template/footer.php
+++ application/views/frontend/template/footer.php
@@ -3,7 +3,7 @@
 		<div class="footer">
 			<div class="container">
 				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.2s">
-					<h2>Multi</h2>
+					<h2><RP>REDES SOCIAIS</RP></h2>
 					<p>
 						In a elit in lorem congue varius. Sed nec arcu.
 						Etiam sit amet augue.
@@ -22,7 +22,7 @@
 				</div>
 
 				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.4s">
-					<h3>RECENT POSTS</h3>
+					<h3>ATIVIDADES RECENTES</h3>
 					<ul>
 						<li>Awesome Design with Overviews</li><hr>
 						<li>Great Design with Features </li><hr>
@@ -32,7 +32,7 @@
 				</div>
 
 				<div class="col-md-4 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
-					<h3>CONTACT INFO</h3>
+					<h3>INFORMAÇÕES</h3>
 					<ul>
 						<li><i class="fa fa-home fa-2x"></i> Office # 38, Suite 54 Elizebth Street, Victoria State Newyork, USA 33026</li><hr>
 						<li><i class="fa fa-phone fa-2x"></i> +38 000 129900</li><hr>
@@ -46,26 +46,8 @@
 		<div class="sub-footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-6">
-						&copy; Multi Theme. All Rights Reserved.
-						<div class="credits">
-							<!--
-								All the links in the footer should remain intact.
-								You can delete the links only if you purchased the pro version.
-								Licensing information: https://bootstrapmade.com/license/
-								Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Multi
-							-->
-							<a href="https://bootstrapmade.com/">Bootstrap Themes</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<ul class="pull-right">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">Services</a></li>
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">Portfolio</a></li>
-							<li><a href="#">Contact</a></li>
-						</ul>
+					<div class="col-md-12">
+						&copy; Voluta Soluções Digitais
 					</div>
 				</div>
 				<div class="pull-right">
