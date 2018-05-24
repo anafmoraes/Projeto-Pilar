719f7a4ca5b8aabc7398bb5b352666a36426d16e
Ana Moraes <ana96moraes@gmail.com>
2018-05-23 23:03:41 -0300

    Estilização da página de Obra - Parte 2

--- application/views/frontend/obra/Registro_View.php
+++ application/views/frontend/obra/Registro_View.php
@@ -33,129 +33,204 @@
         <div class="col-md-12">
           <h1 class="text-center"> <?php echo "Obra ". $obra->nome_objeto ?> </h1><br>
         </div>
-
-        <div class="col-md-6">
-          <div class="row">
-            <div class="col-sm-6">
-              Nome: <?php echo $obra->nome_objeto ?>
-            </div> 
-            <div class="col-sm-6">
-              Título: <?php echo $obra->titulo ?>
-            </div> 
-            <div class="col-sm-6">
-              Numero atual: <?php echo $obra->num_atual ?>
-            </div>
-            <div class="col-sm-6">
-              Número Anterior: <?php echo $obra->num_anterior ?>
-            </div>
+        <div class="col-md-12">     
+          <div class="row" id="caixa">
+            <label> Localização:</label><br>
             <div class="col-sm-12">
-              Fichas Relacionadas: <?php echo $obra->fichas_relacionadas ?>  
+              <?php echo $obra->localizacao ?>
+            </div>
+          </div><br>
+
+          <div class="row" id="caixa">
+            <label> Identificação do bem:</label><br>
+            <div class="col-sm-3">
+              <b>Nome: </b><?php echo $obra->nome_objeto ?>
+            </div> 
+            <div class="col-sm-3">
+              <b>Título: </b><?php echo $obra->titulo ?>
+            </div> 
+            <div class="col-sm-3">
+              <b>Numero atual: </b><?php echo $obra->num_atual ?>
+            </div>
+            <div class="col-sm-3">
+              <b>Número Anterior:</b> <?php echo $obra->num_anterior ?>
+            </div>
+            <div class="col-sm-3">
+              <b>Fichas Relacionadas: </b><?php echo $obra->fichas_relacionadas ?>  
             </div>  
-            <div class="col-sm-6">
-              Classe do objeto: <?php echo $obra->classe ?>
+            <div class="col-sm-3">
+              <b>Classe do objeto: </b><?php echo $obra->classe ?>
             </div>
 
-            <div class="col-sm-6">
-              Sub-classe do objeto: <?php echo $obra->sub_classe ?>
+            <div class="col-sm-3">
+              <b>Sub-classe do objeto: </b><?php echo $obra->sub_classe ?>
             </div>
 
-            <div class="col-sm-6">
-              Material: <?php echo $obra->material ?>
+            <div class="col-sm-3">
+              <b>Material: </b><?php echo $obra->material ?>
             </div>
 
-            <div class="col-sm-6">
-              Técnica:<?php echo $obra->tecnica ?>
+            <div class="col-sm-3">
+              <b>Técnica: </b><?php echo $obra->tecnica ?>
             </div>
 
-            <div class="col-sm-6">
-              Instrições das marcas: <?php echo $obra->inscricoes_marcas ?>
+            <div class="col-sm-3">
+              <b>Instrições/marcas: </b><?php echo $obra->inscricoes_marcas ?>
             </div>
 
-            <div class="col-sm-6">
-              Época: <?php echo $obra->epoca ?>
+            <div class="col-sm-3">
+              <b>Época: </b><?php echo $obra->epoca ?>
             </div>
 
-            <div class="col-sm-6">
-              Autoria: <?php echo $obra->autoria ?>
+            <div class="col-sm-3">
+              <b>Autoria: </b><?php echo $obra->autoria ?>
             </div>
 
-            <div class="col-sm-6">
-              Origem: <?php echo $obra->origem ?>
+            <div class="col-sm-3">
+              <b>Origem: </b><?php echo $obra->origem ?>
             </div>
 
-            <div class="col-sm-6">
-              Procedência: <?php echo $obra->procedencia ?>
+            <div class="col-sm-3">
+              <b>Procedência: </b><?php echo $obra->procedencia ?>
             </div>
 
-            <div class="col-sm-6">
-               Conservação: <?php echo $obra->conservacao ?>
+            <div class="col-sm-3">
+              <b>Conservação: </b><?php echo $obra->conservacao ?>
+            </div>
+          </div><br>
+
+          <div class="row" id="caixa">
+            <label> Dimensões:</label><br>
+            <div class="col-sm-3">
+              <b>Altura: </b><?php echo $obra->altura ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Largura: </b><?php echo $obra->largura ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Comprimento: </b><?php echo $obra->comprimento ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Profundidade: </b><?php echo $obra->profundidade ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Diâmetro: </b><?php echo $obra->diametro ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Peso: </b><?php echo $obra->peso ?>
+            </div>
+
+            <div class="col-sm-3">
+              <b>Circunferência: </b><?php echo $obra->circunferencia ?>
+            </div>
+          </div><br>
+
+          <div class="row" id="caixa">
+            <label> Análise:</label><br>
+            <div class="col-sm-12">
+              <b>Descrição do objeto: </b><?php echo $obra->descricao_objeto ?>
             </div>
 
             <div class="col-sm-12">
-              Localização: <?php echo $obra->localizacao ?>
-            </div>
-
-            <div class="col-sm-6">
-              Altura: <?php echo $obra->altura ?>
-            </div>
-
-            <div class="col-sm-6">
-              Largura: <?php echo $obra->largura ?>
-            </div>
-
-            <div class="col-sm-6">
-              Comprimento: <?php echo $obra->comprimento ?>
-            </div>
-
-            <div class="col-sm-6">
-              Profundidade: <?php echo $obra->profundidade ?>
-            </div>
-
-            <div class="col-sm-6">
-              Diâmetro: <?php echo $obra->diametro ?>
-            </div>
-
-            <div class="col-sm-6">
-              Peso: <?php echo $obra->peso ?>
-            </div>
-
-            <div class="col-sm-6">
-              Circunferência: <?php echo $obra->circunferencia ?>
+              <b>Características técnicas: </b><?php echo $obra->carac_tecnica ?>
             </div>
 
             <div class="col-sm-12">
-              Técnico Responsável pelo preenchimento<?php echo $obra->resp_preenc_tec ?>
+              <b>Características inconográficas: </b><?php echo $obra->carac_inconografica ?>
             </div>
 
             <div class="col-sm-12">
-              Nome do fotógrafo: <?php echo $obra->nome_fotografo ?>
+              <b>Características estilisticas: </b><?php echo $obra->carac_estilistica ?>
             </div>
 
             <div class="col-sm-12">
-              Responsável pela digitação: <?php echo $obra->resp_digitacao ?>
+              <b>Dados históricos: </b><?php echo $obra->dados_historicos ?>
+            </div>
+          </div><br>
+
+          <div class="row" id="caixa">
+            <label> Observações</label><br>
+            <div class="col-sm-6">
+              <b>Publicação:  </b><?php echo $obra->publicacao ?>
             </div>
 
             <div class="col-sm-6">
-              
+              <b>Fontes primárias:  </b><?php echo $obra->fontes_primarias ?>
             </div>
 
             <div class="col-sm-6">
-              
+              <b>Fontes bibliográficas: </b><?php echo $obra->fontes_bib ?>
             </div>
 
             <div class="col-sm-6">
-              
+              <b>Modo de aquisição: </b><?php echo $obra->modo_aquisicao ?>
             </div>
 
-          </div>
+            <div class="col-sm-6">
+              <b>Data da aquisição: </b><?php echo $obra->data_aquisicao ?>
+            </div>
+          </div><br>
+
+          <div class="row" id="caixa">
+            <label> Preenchimento técnico</label><br>
+            <div class="col-sm-12">
+              <b>Técnico Responsável pelo preenchimento técnico: </b><?php echo $obra->resp_preenc_tec ?>
+            </div>
+
+            <div class="col-sm-12">
+              <b>Nome do fotógrafo: </b><?php echo $obra->nome_fotografo ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b>Responsável pela digitação: </b><?php echo $obra->resp_digitacao ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b> Data da digitação: </b><?php echo $obra->data_digitacao ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b>Responsável pela revisão: </b> <?php echo $obra->resp_revisao ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b>Data da revisão:  </b><?php echo $obra->data_revisao ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b>Responsável pela alteração: </b><?php echo $obra->resp_alteracao ?>
+            </div>
+
+            <div class="col-sm-5">
+              <b>Data da alteração: </b><?php echo $obra->data_alteracao ?>
+            </div>
+            <div class="col-sm-12">
+              <b>Responsável pelo cadastro: </b>
+              <?php 
+                $this->db->where('id_funcionario', $obra->id_funcionario);
+                $query = $this->db->get('funcionario');
+                foreach ($query->result() as $row) 
+                {
+                    $nomefunc = $row->nome;
+                    
+                }
+                  echo $nomefunc;
+
+                } ?>
+            </div>
+          </div><br>
         </div>
 
-        <div class="col-md-offset-7">
-          <div class="row">
-            <img src="<?php echo base_url('/assets/img/default-image.png');?>" class="img-fluid" width=500px>
-          </div>
-          <br>
-          <div class="row">
+      </div>
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+            <img src="<?php echo base_url('/assets/img/default-image.png');?>" class="img-fluid" width=550px>
+          <br><br>
             <div class="btn-toolbar" role="toolbar" aria-label="Toolbar de botões">
             <div class="btn-group mr-2" role="group">
               <!-- Inicio do codigo de visualizar exposições -->
@@ -174,44 +249,10 @@
               <!-- Fim do codigo de visualizar restaurações -->
             </div>
           </div>
-          </div>
-        </div>
-          
-          
-          
-          
-          
-          Data da digitação: <?php echo $obra->data_digitacao ?>
-          Responsável pela revisão: <?php echo $obra->resp_revisao ?>
-          Data da revisão: <?php echo $obra->data_revisao ?>
-          Responsável pela alteração: <?php echo $obra->resp_alteracao ?>
-          Data da alteração: <?php echo $obra->data_alteracao ?>
-          Descrição do objeto: <?php echo $obra->descricao_objeto ?>
-          Características técnicas: <?php echo $obra->carac_tecnica ?>
-          Características inconográficas:<?php echo $obra->carac_inconografica ?>
-          Características estilisticas<?php echo $obra->carac_estilistica ?>
-          Dados históricos<?php echo $obra->dados_historicos ?>
-          Publicação: <?php echo $obra->publicacao ?>
-          Fontes primárias: <?php echo $obra->fontes_primarias ?>
-          Fontes bibliográficas: <?php echo $obra->fontes_bib ?>
-          Modo de aquisição: <?php echo $obra->modo_aquisicao ?>
-          Data da aquisição: <?php echo $obra->data_aquisicao ?>
-        
-          Responsável pelo cadastro:
-          <?php 
-            $this->db->where('id_funcionario', $obra->id_funcionario);
-            $query = $this->db->get('funcionario');
-            foreach ($query->result() as $row) 
-            {
-                $nomefunc = $row->nome;
-                
-            }
-
-           echo $nomefunc ?>
-        </p>
-      <?php
-      }?>
+        </div> 
     </div>
+
+
     <!-- Modal de exclusão de obra -->
     <div class="modal fade" id="myModal" role="dialog">
         <div class="modal-dialog modal-sm">
